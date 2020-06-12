7a43c4feb8de3fccd562bd12976f59db61c89e96
Justin <ponthierjustin@gmail.com>
2020-06-11 19:09:03 -0500

    making foundation of html and adding js functions

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-var questionsArray = []
+const questions = document.getElementById("#questions");
+const choiceOne = document.getElementById("#choice1");
+const choiceTwo = document.getElementById("#choice2");
+const choiceThree = document.getElementById("#choice3");
+const choiceFour = document.getElementById("#choice4");
+const startButton = document.getElementById("#start-button");
+
+
+
+
+
+
+
+
+
+
+
+let question = [
+    {
+        questions: "Choose a built-in method used in JavaScript",
+        choiceOne: ""
+
+    }
+]
+
+
+
+
+
+/* // start button function
+startButton.addEventListener("click",)
+// start function starts quiz
+function startQuiz(); */
